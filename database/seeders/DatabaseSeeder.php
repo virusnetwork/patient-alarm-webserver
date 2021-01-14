030a47bc198f605ba3a705b5aca1867e858c6c5a
Miles Singleton <milessingleton@hotmail.co.uk>
2021-01-14 10:29:32 +0000

    added seeders to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            AlarmSeeder::class,
+            BedSeeder::class,
+            PatientSeeder::class,
+            RoomSeeder::class,
+            WardSeeder::class,
+        ]);
     }
 }
