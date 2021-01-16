1cdebb6b986b406c68f691e22cfcce5f383a669c
Miles Singleton <milessingleton@hotmail.co.uk>
2021-01-16 09:16:16 +0000

    improved css

--- resources/views/nurse/mainpage.blade.php
+++ resources/views/nurse/mainpage.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app2')
-@section('ward')
-    Hospital Ward Name
-@endsection
-@section('body')
-    @foreach ($rooms->sortBy('id') as $room)
-    <div id='main'>
-            <table style="width:10%" >
-                <tr>
-                    <th>Room {{ $room->id }} </th>
-                </tr>
-                @foreach ($beds as $bed)
-                    @if ($bed->room_id == $room->id)
-                        <tr>
-                            <td>{{ $bed->id }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-            </table>
-        </div>
-    @endforeach
 
+@section('page_name')
+@endsection
+
+
+@section('body')
+
+    @if (session('message'))
+        <p><b>{{ session('message') }}</b></p>
+    @endif
+    <div class="flex flex-wrap -m-4">
+        @foreach ($rooms->sortBy('id') as $room)
+            <div class="p-4 md:w-1/3"">
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <table style="width:10%">
+                        <tr>
+                            <th>Room {{ $room->id }} </th>
+                        </tr>
+                        @foreach ($beds as $bed)
+                            @if ($bed->room_id == $room->id)
+                                <tr>
+                                    <td>{{ $bed->id }}</td>
+                                </tr>
+                            @endif
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 @endsection
